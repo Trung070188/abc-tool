9323732f8571a4fcc2bc217610e893fd39f08b18
Trung <trung.nguyen@blueocean.net.vn>
2023-05-12 16:48:12 +0700

    update

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
 
                 <form method="POST" action="/task">
-                    <label>Add link theo Shop</label>
+                    <label>Add link theo shop</label>
 
                     <div class="form-group">
                         <textarea name="description" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"  placeholder='Enter your link etsy'></textarea>
@@ -28,7 +28,7 @@
 
                 <form method="POST" action="/task/categories">
                     <div class="form-group mt-5">
-                        <label>Add Link theo ngách</label>
+                        <label>Add link theo danh mục</label>
                         <textarea name="categories" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"  placeholder='Enter your link etsy'></textarea>
                         @if ($errors->has('categories'))
                             <span class="text-danger">{{ $errors->first('categories') }}</span>
